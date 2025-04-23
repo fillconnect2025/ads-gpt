0891d656e15fb3ac40ada12fac936d19a37c7ef4
andersonalvesoelho <a0a0coelho0@gmail.com>
2025-04-23 19:58:21 -0300

    lovable

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
-import path from 'path';
-import { defineConfig } from 'vite';
+
+import react from "@vitejs/plugin-react-swc";
+import { componentTagger } from "lovable-tagger";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./",
   server: {
-    host: '::',
+    host: "::",
     port: 8080,
   },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
